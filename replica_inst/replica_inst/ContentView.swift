bed87bd99478a0340c467ec0f0f19a09d210aa80
Denis <denismerkotun@gmail.com>
2021-04-05 21:09:08 +0300

    first commit

---
+++ replica_inst/replica_inst/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  replica_inst
+//
+//  Created by  Denis on 05.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ replica_inst/replica_inst/replica_instApp.swift
@@ -0,0 +1,17 @@
+//
+//  replica_instApp.swift
+//  replica_inst
+//
+//  Created by  Denis on 05.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct replica_instApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
